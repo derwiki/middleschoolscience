dde44cdc2a7487eafaaf783f434cf64d212a33ca
Adam Derewecki <derewecki@gmail.com>
2014-10-19 21:04:19 -0700

    Add homepage intro and basic SEO

--- Gemfile
+++ Gemfile
@@ -10,10 +10,7 @@ gem 'sass-rails', '~> 5.0.0.beta1'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'json'
-
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
-
+gem 'meta-tags-helpers'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,6 +3,7 @@ class LessonsController < ApplicationController
     @sections = %w(what_is_electricity conductors_and_insulators circuits wire
                    switches wire_a_house make_a_motor ac_dc make_a_battery
                    fruit_battery thermocouple)
+    set_meta title: 'Middle School Science Rocks! Electricity for Kids'
   end
 
   def matter
@@ -10,24 +11,34 @@ class LessonsController < ApplicationController
     @sections = %w(forms_of_matter structure_of_an_atom
                    transferring_of_electrons combining_atoms
                    how_many_molecules_and_atoms)
+    set_meta title: 'Middle School Science Rocks! Matter'
   end
 
   def rock_cycle
     @sections = []
+    set_meta title: 'Middle School Science Rocks! Rock Cycle'
   end
 
   def weather_atmosphere
     @sections = %w(weather_atmosphere solar_energy special_winds pressure
                    humidity storms uses_of_this_information)
+    set_meta title: 'Middle School Science Rocks! Weather and Atmosphere'
   end
 
   def igneous_rock
     @secionts = []
+    set_meta title: 'Middle School Science Rocks! Igneous Rocks'
   end
 
   def index
+    set_meta title: <<-EOS.squish
+      Middle School Science Rocks!
+      Science lesson plans,
+      Cool kids science experiments
+    EOS
   end
 
   def authors
+    set_meta title: 'Middle School Science Rocks! About the authors'
   end
 end
