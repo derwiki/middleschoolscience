ea14edc13738fda7f48b13ccb1ec0dda6aa5ec49
Adam Derewecki <adam@snapdocs.com>
2016-12-24 14:22:37 -0800

    Update Adam's pic

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,16 +4,30 @@ class LessonsController < ApplicationController
   before_action :seo
 
   def electricity
-    @sections = %w(what_is_electricity conductors_and_insulators circuits wire
-                   switches wire_a_house make_a_motor ac_dc make_a_battery
-                   fruit_battery thermocouple)
+    @sections = %w(
+      what_is_electricity
+      conductors_and_insulators
+      circuits
+      wire
+      switches
+      wire_a_house
+      make_a_motor
+      ac_dc
+      make_a_battery
+      fruit_battery
+      thermocouple
+    )
   end
 
   def matter
     require 'periodic' # for displaying
-    @sections = %w(forms_of_matter structure_of_an_atom
-                   transferring_of_electrons combining_atoms
-                   how_many_molecules_and_atoms)
+    @sections = %w(
+      forms_of_matter
+      structure_of_an_atom
+      transferring_of_electrons
+      combining_atoms
+      how_many_molecules_and_atoms
+    )
   end
 
   def rock_cycle
@@ -21,8 +35,15 @@ class LessonsController < ApplicationController
   end
 
   def weather_atmosphere
-    @sections = %w(weather_atmosphere solar_energy special_winds pressure
-                   humidity storms uses_of_this_information)
+    @sections = %w(
+      weather_atmosphere
+      solar_energy
+      special_winds
+      pressure
+      humidity
+      storms
+      uses_of_this_information
+    )
   end
 
   def igneous_rock
@@ -42,7 +63,6 @@ class LessonsController < ApplicationController
   end
 
   def authors
-    set_meta title: 'Middle School Science Rocks! About the authors'
   end
 
   def sitemap
